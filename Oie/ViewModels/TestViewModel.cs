5998f605e966ac6011cf2164a878c730049a90a9
thomaj46 <thomaj46@uwosh.edu>
2016-05-06 17:25:17 -0500

    Added more examples in test view.

--- Oie/ViewModels/TestViewModel.cs
+++ Oie/ViewModels/TestViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Oie.DataAccess;
+using Oie.DataAccess.DbSets;
 using Oie.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -16,18 +18,20 @@ namespace Oie.ViewModels
         {
             this.DbContext = new OieDbContext();
             this.TestCommand = new DelegateCommand(this.TestEf);
+            this.Students = new ObservableCollection<Student>();
         }
 
         public ICommand TestCommand { get; set; }
+
+        public ObservableCollection<Student> Students { get; set; }
 
         private OieDbContext DbContext { get; set; }
 
         public void TestEf()
         {
-            var students = this.DbContext
-                .Students
-                .Where(dept => dept.FirstName.StartsWith("J"));
-
+            IEnumerable<Student> students = this.DbContext.Students.Take(50).ToList();
+            this.Students.Clear();
+            this.Students.AddRange(students);
             foreach(var student in students)
             {
                 Debug.WriteLine(student.EmailPrimary);

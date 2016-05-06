5105426eec42adb46557b17a77c9e0f462441cc4
blavigne13 <blavigne13@gmail.com>
2016-05-06 13:52:45 -0500

    test db cxn

---
+++ Oie/DataAccess/MySqlDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace Oie.DataAccess
+{
+    public class MySqlDatabase
+    {
+        public void Test()
+        {
+            var cs = "server=labdb.acs.uwosh.edu;uid=thomaj46;pwd=0455446;database=thomaj46;";
+            using (var con = new MySqlConnection(cs))
+            {
+                var query = "select * from fakedepts";
+                con.Open();
+                var command = new MySqlCommand(query, con);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Debug.WriteLine(reader[0]);
+                }
+            }
+        }
+    }
+}
---
+++ Oie/ViewModels/TestViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Oie.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Oie.ViewModels
+{
+    public class TestViewModel
+    {
+        public TestViewModel()
+        {
+            this.TestCommand = new MyCommand();
+        }
+
+        public ICommand TestCommand { get; set; }
+
+        private class MyCommand : ICommand
+        {
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                new MySqlDatabase().Test();
+            }
+        }
+    }
+}

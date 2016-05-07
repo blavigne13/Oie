3098e37a9f2c4003b209d0ac808293a44438b723
thomaj46 <thomaj46@uwosh.edu>
2016-05-06 23:33:02 -0500

    Use case 2 (6) complete.

--- Oie/DataAccess/OieDbContext.cs
+++ Oie/DataAccess/OieDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,14 @@ namespace Oie.DataAccess
             : base("OieDbContext")
         {
             Database.SetInitializer<OieDbContext>(new CreateDatabaseIfNotExists<OieDbContext>());
+            Database.Log = (log) => Debug.WriteLine(log);
         }
 
         public DbSet<FakeDepartment> FakeDepartments { get; set; }
 
         public DbSet<Student> Students { get; set; }
+
+        public DbSet<AirportPickup> AirportPickups { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
--- Oie/Views/UseCase2View.xaml.cs
+++ Oie/Views/UseCase2View.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oie.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,8 @@ namespace Oie.Views
     {
         public UseCase2View()
         {
-            InitializeComponent();
+            this.DataContext = new UseCase2ViewModel();
+            this.InitializeComponent();
         }
     }
 }
